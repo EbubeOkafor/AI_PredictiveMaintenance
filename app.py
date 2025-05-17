3cb8196a7a346e836d9f4fad41fa8ddd96b465c0
Ebube Okafor <okaforebube8@gmail.com>
2025-05-18 00:52:37 +0100

    Uploaded my streamlit app/dashboard

---
+++ app.py
@@ -0,0 +1,22 @@
+import joblib
+import streamlit as st
+import numpy as np
+
+model = joblib.load("model.joblib")
+
+st.title('Predictive Maintenance Alert Dashboard')
+
+Vibration = st.number_input("Enter vibration value")
+Temperature = st.number_input("Enter temperature value")
+Pressure = st.number_input("Enter pressure value")
+RPM = st.number_input("Enter RPM value")
+Runtime = st.number_input("Enter Runtime value")
+
+if st.button("Predict Maintenance Need"):
+    input_data = np.array([[Vibration, Temperature, Pressure, RPM, Runtime]])
+    prediction = model.predict(input_data)
+    if prediction[0] == 1:
+        st.error('Maintenance Needed!')
+    else:
+        st.success('No Maintenance Needed')
+
